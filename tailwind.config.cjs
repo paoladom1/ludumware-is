cbb2379ce54bcfb30487c32d834e7972dbe61c90
Paola Dominguez <paoladomdiaz@gmail.com>
2022-09-28 05:05:22 -0600

    [LUD-85] view: login

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: [
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+    },
+    extend: {
+      colors: {
+        facebook: "#3B5998",
+        google: "#DB3236",
+        discord: "#7289DA",
+      },
+      height: {
+        "vh": "100vh"
+      }
+    },
   },
   plugins: [],
 };
