54ff12c5a32de80c5a904c9db31fc005de1191ba
Paola Dominguez <paoladomdiaz@gmail.com>
2022-10-04 01:11:52 -0600

    fix spacing in button content

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,49 +3,50 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faFacebook,
-    faGoogle,
-    faDiscord,
+  faFacebook,
+  faGoogle,
+  faDiscord,
 } from "@fortawesome/free-brands-svg-icons";
 
 const Login: NextPage = () => {
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    return (
-        <div className="h-vh w-full bg-gray-200 flex justify-center items-center">
-            {session ? (
-                <div>
-                    <p>hi {session.user?.name}</p>
+  return (
+    <div className="h-vh w-full bg-gray-200 flex justify-center items-center">
+      {session ? (
+        <div>
+          <p>hi {session.user?.name}</p>
 
-                    <button onClick={() => signOut()}>Logout</button>
-                </div>
-            ) : (
-                <div className="bg-white flex flex-col justify-center items-center p-8 m-8">
-                    <p className="text-blue-400 font-bold text-4xl">LUDUMWARE</p>
-                    <p className="text-gray-500 text-2xl font-normal my-7">
-                        Inicia sesión
-                    </p>
-                    <button className="w-80 bg-facebook text-white text-l py-2 px-5 mb-3"
-                        onClick={() => signIn("facebook")}>
-                        <FontAwesomeIcon className="text-xl mr-2" icon={faFacebook} />
-                        Inicia sesión con Facebook
-                    </button>
-                    <button className="w-80 bg-google text-white text-l py-2 px-5 mb-3"
-                        onClick={() => signIn("google")}>
-                        <FontAwesomeIcon className="text-xl mr-2" icon={faGoogle} />
-                        Inicia sesión con Google
-                    </button>
-                    <button
-                        className="w-80 bg-discord text-white text-l py-2 px-5"
-                        onClick={() => signIn("discord")}
-                    >
-                        <FontAwesomeIcon className="text-xl mr-2" icon={faDiscord} />
-                        Inicia sesión con Discord
-                    </button>
-                </div>
-            )}
+          <button onClick={() => signOut()}>Logout</button>
         </div>
-    );
+      ) : (
+        <div className="bg-white flex flex-col justify-center items-center p-8 m-8">
+          <p className="text-blue-400 font-bold text-4xl">LUDUMWARE</p>
+          <p className="text-gray-500 text-2xl font-normal my-7">
+            Inicia sesión
+          </p>
+          <button className="w-80 bg-facebook text-white text-l py-2 px-5 mb-3 flex flex-row justify-evenly">
+            <FontAwesomeIcon className="text-xl pl-1.5" icon={faFacebook} />
+            <span>Inicia sesión con Facebook</span>
+          </button>
+          <button
+            className="w-80 bg-google text-white text-l py-2 px-5 mb-3 flex flex-row justify-evenly"
+            onClick={() => signIn("google")}
+          >
+            <FontAwesomeIcon className="text-xl" icon={faGoogle} />
+            <span>Inicia sesión con Google</span>
+          </button>
+          <button
+            className="w-80 bg-discord text-white text-l py-2 px-5 flex flex-row justify-evenly"
+            onClick={() => signIn("discord")}
+          >
+            <FontAwesomeIcon className="text-xl" icon={faDiscord} />
+            <span>Inicia sesión con Discord</span>
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Login;
