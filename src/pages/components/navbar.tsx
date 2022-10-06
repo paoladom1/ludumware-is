365b8d6dc36157e9e4b7ceb205d2e4766f64f02e
Paola Dominguez <paoladomdiaz@gmail.com>
2022-10-06 02:56:13 -0600

    navbar updates

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const { data: session } = useSession({ required: true });
 
   return (
-    <nav className="bg-stone-100 sm:px-4 py-4 dark:bg-gray-900 w-full top-0 left-0 border-b border-gray-200 dark:border-gray-600 mb-8">
+    <nav className="bg-white sm:px-4 py-4 dark:bg-gray-900 w-full top-0 left-0 border-b border-gray-200 dark:border-gray-600 mb-8">
       <div className="flex flex-wrap justify-between items-center px-4">
         <a href="#" className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
@@ -12,11 +12,13 @@ const Navbar = () => {
           </span>
         </a>
         <div className="flex md:order-2 align-center h-full">
-          <span className="h-full mr-4">{session?.user?.email}</span>
+          <span className="h-full mr-4 self-center">
+            {session?.user?.email}
+          </span>
           <button
             onClick={() => signOut()}
             type="button"
-            className="border rounded p-2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="bg-blue-300 rounded p-2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Cerrar Sesión
           </button>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,25 +9,34 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 const Login: NextPage = () => {
+  const login =
+    (provider: string) => (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      signIn(provider, { callbackUrl: "/" });
+    };
+
   return (
     <div className="h-vh w-full bg-gray-200 flex justify-center items-center">
       <div className="bg-white flex flex-col justify-center items-center p-8 m-8">
         <p className="text-blue-400 font-bold text-4xl">LUDUMWARE</p>
         <p className="text-gray-500 text-2xl font-normal my-7">Inicia sesión</p>
-        <button className="w-80 bg-facebook text-white text-l py-2 px-5 mb-3 flex flex-row justify-evenly">
+        <button
+          className="w-80 bg-facebook text-white text-l py-2 px-5 mb-3 flex flex-row justify-evenly"
+          onClick={login("facebook")}
+        >
           <FontAwesomeIcon className="text-xl pl-1.5" icon={faFacebook} />
           <span>Inicia sesión con Facebook</span>
         </button>
         <button
           className="w-80 bg-google text-white text-l py-2 px-5 mb-3 flex flex-row justify-evenly"
-          onClick={() => signIn("google", { callbackUrl: "/" })}
+          onClick={login("google")}
         >
           <FontAwesomeIcon className="text-xl" icon={faGoogle} />
           <span>Inicia sesión con Google</span>
         </button>
         <button
           className="w-80 bg-discord text-white text-l py-2 px-5 flex flex-row justify-evenly"
-          onClick={() => signIn("discord", { callbackUrl: "/" })}
+          onClick={login("discord")}
         >
           <FontAwesomeIcon className="text-xl" icon={faDiscord} />
           <span>Inicia sesión con Discord</span>
