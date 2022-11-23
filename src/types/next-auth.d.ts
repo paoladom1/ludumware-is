3cc5237f5b32ab8836a6c1d1e0009ae51ebaabb5
Paola Dominguez <paoladomdiaz@gmail.com>
2022-11-22 23:56:21 -0600

    fix type error on nextauth user

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,6 +2,9 @@ import { UserRole } from "@prisma/client";
 import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    role: UserRole;
+  }
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
